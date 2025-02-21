3a63aa777a4e7f03e3d3ee517e5fc650ae160e02
gxnzalx55 <gonosusal@alum.us.es>
2025-02-21 12:25:00 +0100

    aaa

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Inicializando index.js');
+const express = require("express");
+const app = express();
+const PORT = 16078;
+const cool = require("cool-ascii-faces");
+
+app.use("/",express.static("./public"));
+
+app.get("/hello", (req, res)=>{res.send("Welcome to the server!")});
+app.get("/cool", (req, res)=>{res.send(cool())});
+
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT} `+cool())});
+
+console.log(`finished setup`);
