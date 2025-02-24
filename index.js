19dc23e353bec31c882c580acf18dc42b6cf0667
gxnzalx55 <gonosusal@alum.us.es>
2025-02-24 17:10:48 +0100

    pruebas

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const cool = require("cool-ascii-faces");
 const app = express();
 const PORT = process.env.PORT || 16078;
 
-app.use("/",express.static("./public"));
+app.use("/",express.static(__dirname));
 
-app.get("/hello", (req, res)=>{res.send("Welcome to the server!")});
-app.get("/cool", (req, res)=>{res.send(cool())});
+app.get("/hello", (req, res)=>{
+    res.send("Welcome to the server!")});
 
-app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT} `+cool())});
+app.get("/cool", (req, res)=>{
+    res.send(cool())});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT} `+cool())});
 
 console.log(`finished setup`);
